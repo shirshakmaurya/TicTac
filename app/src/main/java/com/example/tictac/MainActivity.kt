d1b7be3d4fd27d814846404de14d02a9632fdeaa
shirshak <shirshakmaurya63@gmail.com>
2021-05-23 12:24:58 +0530

    initial

--- app/src/main/java/com/example/tictac/MainActivity.kt
+++ app/src/main/java/com/example/tictac/MainActivity.kt
@@ -11,16 +11,16 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btn1 = findViewById<Button>(R.id.btn1)
-        var btn2 = findViewById<Button>(R.id.btn2)
-        var btn3 = findViewById<Button>(R.id.btn3)
-        var btn4 = findViewById<Button>(R.id.btn4)
-        var btn5 = findViewById<Button>(R.id.btn5)
-        var btn6 = findViewById<Button>(R.id.btn6)
-        var btn7 = findViewById<Button>(R.id.btn7)
-        var btn8 = findViewById<Button>(R.id.btn8)
-        var btn9 = findViewById<Button>(R.id.btn9)
-        var btnReset = findViewById<Button>(R.id.btnReset)
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn3 = findViewById<Button>(R.id.btn3)
+        val btn4 = findViewById<Button>(R.id.btn4)
+        val btn5 = findViewById<Button>(R.id.btn5)
+        val btn6 = findViewById<Button>(R.id.btn6)
+        val btn7 = findViewById<Button>(R.id.btn7)
+        val btn8 = findViewById<Button>(R.id.btn8)
+        val btn9 = findViewById<Button>(R.id.btn9)
+        val btnReset = findViewById<Button>(R.id.btnReset)
         board = arrayOf(
             arrayOf(btn1,btn2,btn3),
             arrayOf(btn4,btn5,btn6),
